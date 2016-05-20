936c6e50bd8ddee53ddf8905041f96d66a7a97bc
Kane Meyer <kanomax@gmail.com>
2016-05-20 12:53:50 -0500

    added tooltip, and mixed controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,124 +36,129 @@ var cfg2 = {
         return this.tableId + ' tbody';
     }
 };
+ready = function() {
 
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
 
-
-
-function select_agent_fields() {
-	$('#new-agent-fields').modal('show');
-}
-
-function remove_fields(link) {
-    $(link).prev("input[type=hidden]").val("1");
-    $(link).closest(".fields").hide();
-}
- 
-function add_fields(link, association, content) {
-    var new_id = new Date().getTime();
-    var regex = new RegExp("new_" + association, "g");
-    $(link).parent().after(content.replace(regex, new_id));
-    jQuery('#new-housefile-fields').modal('show');
-}
-
-var housefileFieldsUI = {
-    init: function() {
-        $('#addButton').on('click', function() {
-            formHandler.appendFields();
-            formHandler.hideForm();
-        });
-        $('#cancelButton').on('click', function() {
-            formHandler.hideForm();
-        });
-        $('#closeButton').on('click', function() {
-            formHandler.hideForm();
-        });
+    function select_agent_fields() {
+        $('#new-agent-fields').modal('show');
     }
-};
 
-var selectFieldsUI = {
-    init: function() {
-        $('#addButton').on('click', function() {
-            selectformHandler.appendFields();
-            selectformHandler.hideForm();
-        });
-        $('#cancelButton').on('click', function() {
-            selectformHandler.hideForm();
-        });
-        $('#closeButton').on('click', function() {
-            selectformHandler.hideForm();
-        });
+    function remove_fields(link) {
+        $(link).prev("input[type=hidden]").val("1");
+        $(link).closest(".fields").hide();
     }
-};
 
-
-var formHandler = {
-    // Public method for adding a new row to the table.
-    appendFields: function() {
-        // Get a handle on all the input fields in the form and detach them from the DOM (we will attach them later).
-        var inputFields = $(cfg.formId + ' ' + cfg.inputFieldClassSelector);
-        inputFields.detach();
- 
-        // Build the row and add it to the end of the table.
-        rowBuilder.addRow(cfg.getTBodySelector(), inputFields);
- 
-        // Add the "Remove" link to the last cell.
-        rowBuilder.link.clone().appendTo($('tr:last td:last'));
-    },
- 
-    // Public method for hiding the data entry fields.
-    hideForm: function() {
-        $(cfg.formId).modal('hide');
+    function add_fields(link, association, content) {
+        var new_id = new Date().getTime();
+        var regex = new RegExp("new_" + association, "g");
+        $(link).parent().after(content.replace(regex, new_id));
+        jQuery('#new-housefile-fields').modal('show');
     }
-};
 
-var selectformHandler = {
-    appendFields: function() {
-        var inputFields = $('#agent_attributes_name');
-		inputFields.val($('input:radio:checked').parent('label').text());
-    },
- 
-    // Public method for hiding the data entry fields.
-    hideForm: function() {
-        $(cfg2.formId).modal('hide');
-    }
-};
+    var housefileFieldsUI = {
+        init: function () {
+            $('#addButton').on('click', function () {
+                formHandler.appendFields();
+                formHandler.hideForm();
+            });
+            $('#cancelButton').on('click', function () {
+                formHandler.hideForm();
+            });
+            $('#closeButton').on('click', function () {
+                formHandler.hideForm();
+            });
+        }
+    };
+
+    var selectFieldsUI = {
+        init: function () {
+            $('#addButton').on('click', function () {
+                selectformHandler.appendFields();
+                selectformHandler.hideForm();
+            });
+            $('#cancelButton').on('click', function () {
+                selectformHandler.hideForm();
+            });
+            $('#closeButton').on('click', function () {
+                selectformHandler.hideForm();
+            });
+        }
+    };
+
+
+    var formHandler = {
+        // Public method for adding a new row to the table.
+        appendFields: function () {
+            // Get a handle on all the input fields in the form and detach them from the DOM (we will attach them later).
+            var inputFields = $(cfg.formId + ' ' + cfg.inputFieldClassSelector);
+            inputFields.detach();
+
+            // Build the row and add it to the end of the table.
+            rowBuilder.addRow(cfg.getTBodySelector(), inputFields);
+
+            // Add the "Remove" link to the last cell.
+            rowBuilder.link.clone().appendTo($('tr:last td:last'));
+        },
+
+        // Public method for hiding the data entry fields.
+        hideForm: function () {
+            $(cfg.formId).modal('hide');
+        }
+    };
+
+    var selectformHandler = {
+        appendFields: function () {
+            var inputFields = $('#agent_attributes_name');
+            inputFields.val($('input:radio:checked').parent('label').text());
+        },
+
+        // Public method for hiding the data entry fields.
+        hideForm: function () {
+            $(cfg2.formId).modal('hide');
+        }
+    };
 
 
 // Provides functionality for building the HTML that represents a new <TR> for the Pilots table.
-var rowBuilder = function() {
-    // Private property that define the default <TR> element text.
-    var row = $('<tr>', { class: 'fields' });
- 
-    // Public property that describes the "Remove" link.
-    var link = $('<a>', {
-        href: '#',
-        onclick: 'remove_fields(this); return false;',
-        title: 'Delete this File.'
-    }).append('Remove');
- 
-    // A private method for building a <TR> w/the required data.
-    var buildRow = function(fields) {
-        var newRow = row.clone();
- 
-        $(fields).map(function() {
-            $(this).removeAttr('class');
-            var td = $('<td/>').append($(this));
-            td.appendTo(newRow);
-        });
- 
-        return newRow;
-    };
- 
-    // A public method for building a row and attaching it to the end of a <TBODY> element.
-    var attachRow = function(tableBody, fields) {
-        var row = buildRow(fields);
-        $(row).appendTo($(tableBody));
-    };
- 
-    // Only expose public methods/properties.
-    return {
-        addRow: attachRow,
-        link: link
-    };
-}();
+    var rowBuilder = function () {
+        // Private property that define the default <TR> element text.
+        var row = $('<tr>', {class: 'fields'});
+
+        // Public property that describes the "Remove" link.
+        var link = $('<a>', {
+            href: '#',
+            onclick: 'remove_fields(this); return false;',
+            title: 'Delete this File.'
+        }).append('Remove');
+
+        // A private method for building a <TR> w/the required data.
+        var buildRow = function (fields) {
+            var newRow = row.clone();
+
+            $(fields).map(function () {
+                $(this).removeAttr('class');
+                var td = $('<td/>').append($(this));
+                td.appendTo(newRow);
+            });
+
+            return newRow;
+        };
+
+        // A public method for building a row and attaching it to the end of a <TBODY> element.
+        var attachRow = function (tableBody, fields) {
+            var row = buildRow(fields);
+            $(row).appendTo($(tableBody));
+        };
+
+        // Only expose public methods/properties.
+        return {
+            addRow: attachRow,
+            link: link
+        };
+    }();
+};
+$(document).ready(ready);
+$(document).on('page:load', ready);
