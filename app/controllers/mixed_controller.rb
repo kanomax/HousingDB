612ffbe3c148b7eb047f60bc0dae0c6cbaadb13f
Kane Meyer <kanomax@gmail.com>
2016-05-25 15:52:35 -0500

    fix salesandlistings

--- app/controllers/mixed_controller.rb
+++ app/controllers/mixed_controller.rb
@@ -10,10 +10,20 @@ class MixedController < ApplicationController
   def salesandlistings
     list = []
     list += Sale.where(house_id: params[:id]).order(:saledate).map do |sale|
-      Posting.new("Sale", sale.price,sale.saledate,sale.agent.name?)
+      Posting.new("Sale", sale.price,sale.saledate,
+                   if sale.agent.nil?
+                     ""
+                   else
+                     sale.agent.name
+                   end)
     end
     list += Listing.where(house_id: params[:id]).order(:listingdate).map do |listing|
-      Posting.new("Listing",listing.listingprice,listing.listingdate,listing.agent.name?)
+      Posting.new("Listing",listing.listingprice,listing.listingdate,
+                  if listing.agent.nil?
+                    ""
+                  else
+                    listing.agent.name
+                  end)
     end
     @postings = list.sort_by(&:date).reverse
     # @search = @postings.search(params[:search])
