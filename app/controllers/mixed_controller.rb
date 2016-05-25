23a427493c7747ddd1c4f22333a6564e00df30fd
Kane Meyer <kanomax@gmail.com>
2016-05-25 14:05:55 -0500

    added tooltip, and mixed controller

---
+++ app/controllers/mixed_controller.rb
@@ -0,0 +1,24 @@
+class MixedController < ApplicationController
+  before_filter :get_house
+
+  def get_house
+    if params[:house_id] != nil
+      @house = House.find(params[:house_id])
+    end
+  end
+
+  def salesandlistings
+    list = []
+    list += Sale.where(house_id: params[:id]).order(:saledate).map do |sale|
+      Posting.new("Sale", sale.price,sale.saledate,sale.agent.name)
+    end
+    list += Listing.where(house_id: params[:id]).order(:listingdate).map do |listing|
+      Posting.new("Listing",listing.listingprice,listing.listingdate,listing.agent.name)
+    end
+    @postings = list.sort_by(&:date).reverse
+    # @search = @postings.search(params[:search])
+    # @sales = @search.result
+  end
+
+
+end
